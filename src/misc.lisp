dc8f4f8dfd44e234cd09e8b5fa012f57a0b359c5
ctoid <funk443@yandex.com>
2023-03-05 19:38:52 +0800

    License notices

--- src/misc.lisp
+++ src/misc.lisp
@@ -1,3 +1,20 @@
+;; Copyright (C) 2023  CToID
+
+;; This file is part of cl-discord
+
+;; cl-discord is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; cl-discord is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public License
+;; along with cl-discord.  If not, see <https://www.gnu.org/licenses/>.
+
 (DEFPACKAGE :CL-DISCORD.MISC
   (:NICKNAMES :CD.MISC)
   (:USE :CL)
